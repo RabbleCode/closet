a26393631fedb16191285e271f2e489add6f64c4
RabbleCode <rabblewarcraft@gmail.com>
2024-06-04 23:49:06 -0700

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # closet
-A simple WoW: Classic addon to create and delete equipment sets
+A simple WoW: Classic addon to create and delete equipment sets.
+
+Usage:
+
+* `/saveset name` - Overwrites the equipment set named "name" with all equipment currently worn, or creates one a set it doesn't yet exist
+* `/deleteset name` or `delset name` - Deletes the equipment set "name" if it exists
+* `/listsets` - Lists all currently saved sets for the current character
+* `/equipset name` - Equips items from the previously saved set named "name"
+
+Notes:
+
+Classic Era WoW API supports the Blizzard equipment manager functionality and the `/equipset` macro command, but the UI options for creating and modifying sets are disabled.
+
+This addon just creates a couple slash commands to easily interface with the existing Blizzard API to allow creating, editing, and deleting sets.
+
+There are no plans to develop more advanced features like integrating this visually into the "paperdoll" character frame or allowing for editing individual slots at a time. This is all or nothing. When you save a set, it saves everything you're currently wearing with the name you chose.
---
+++ closet.lua
@@ -0,0 +1,89 @@
+closet = {};
+
+function closet:OnLoad()
+    SLASH_CLOSET_SAVESET1 = "/saveset";
+    SLASH_CLOSET_DELETESET1 = "/deleteset";
+    SLASH_CLOSET_DELETESET2 = "/delset";
+    SLASH_CLOSET_LISTSETS1 = "/listsets";
+	SlashCmdList["CLOSET_SAVESET"] = function(setName) closet:HandleSaveSet(setName) end
+    SlashCmdList["CLOSET_DELETESET"] = function(setName) closet:HandleDeleteSet(setName) end
+    SlashCmdList["CLOSET_LISTSETS"] = function() closet:HandleListSets() end
+
+	closetFrame:RegisterEvent("PLAYER_LOGIN")
+	closetFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	closetFrame:RegisterEvent("ADDON_LOADED")
+end
+
+function closet:OnEvent(self, event, ...)
+	local arg1, arg2, arg3, arg4 = ...
+	if event == "ADDON_LOADED" and arg1 == "closet" then
+		closetFrame:UnregisterEvent("ADDON_LOADED");
+	elseif event == "PLAYER_LOGIN" then
+		closetFrame:UnregisterEvent("PLAYER_LOGIN");
+		closet:Announce();		
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		closetFrame:UnregisterEvent("PLAYER_ENTERING_WORLD");
+	end
+end
+
+function closet:Announce()
+    local numSets = C_EquipmentSet.GetNumEquipmentSets()
+    if(numSets > 1) then
+        closet:PrintMessageWithClosetPrefix("activated. "..numSets.." sets found.");
+    elseif(numSets == 1) then
+        closet:PrintMessageWithClosetPrefix("activated. "..numSets.." set found.");
+    else
+	    closet:PrintMessageWithClosetPrefix("activated. No sets found.");
+    end
+end
+
+function closet:PrintMessageWithClosetPrefix(message)
+    closet:PrintMessage(YELLOW_FONT_COLOR_CODE.."CLOSET|r | "..message)
+end
+    
+function closet:PrintMessage(message)
+    DEFAULT_CHAT_FRAME:AddMessage(message)
+end
+
+function closet:HandleSaveSet(setName)
+    if setName ~= nil and setName ~= "" then        
+        local setID = C_EquipmentSet.GetEquipmentSetID(setName)
+        if(setID == nil) then
+            C_EquipmentSet.CreateEquipmentSet(setName)
+            closet:PrintMessage(GREEN_FONT_COLOR_CODE.."Created set|r "..setName)
+        else 
+            C_EquipmentSet.SaveEquipmentSet(setID)
+            closet:PrintMessage(GREEN_FONT_COLOR_CODE.."Saved set|r "..setName)
+        end
+     else
+         closet:PrintMessage(RED_FONT_COLOR_CODE.."Save set failed|r - No set name provided")
+     end
+end
+
+function closet:HandleDeleteSet(setName)
+    if setName ~= nil and setName ~= "" then    
+        local setID = C_EquipmentSet.GetEquipmentSetID(setName)
+        if(setID ~= nil) then
+            C_EquipmentSet.DeleteEquipmentSet(setID)
+            closet:PrintMessage(GREEN_FONT_COLOR_CODE.."Deleted set|r "..setName)
+        else
+            closet:PrintMessage(RED_FONT_COLOR_CODE.."Delete set failed|r - No set found named "..setName)
+        end
+    else
+        closet:PrintMessage(RED_FONT_COLOR_CODE.."Delete set failed|r - No set name provided")
+    end
+end
+
+function closet:HandleListSets() 
+    local numSets = C_EquipmentSet.GetNumEquipmentSets()
+    if(numSets > 0) then
+        closet:PrintMessage("Listing all sets...")
+        
+        for _,id in ipairs(C_EquipmentSet.GetEquipmentSetIDs()) do 
+            local name, _, setID, isEquipped = C_EquipmentSet.GetEquipmentSetInfo(id)
+            closet:PrintMessage("  "..name)
+        end
+    else
+        closet:PrintMessage("This character has no sets.")
+    end
+end
